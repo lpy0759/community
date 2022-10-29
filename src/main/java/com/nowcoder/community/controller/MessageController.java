cb2bbb4cf654be518af2fbc309cb7267cb77d537
lpy0759 <2533906024@qq.com>
2022-10-29 08:44:03 +0800

    统一处理异常

--- src/main/java/com/nowcoder/community/controller/MessageController.java
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -31,6 +31,7 @@ public class MessageController {
     // 私信列表
     @RequestMapping(path = "/letter/list",method = RequestMethod.GET)
     public String getLetterList(Model model, Page page){
+
         User user = hostHolder.getUser();
         // 分页信息
         page.setLimit(5);
@@ -130,6 +131,7 @@ public class MessageController {
     @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
     @ResponseBody
     public String sendLetter(String toName, String content){
+        Integer.valueOf("abc");
         User target = userService.findUserByName(toName);
         if(target == null){
             return CommunityUtil.getJSONString(1, "目标用户不存在！");
